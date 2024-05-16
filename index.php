bc887a201fe2a77482605f07521ac2c5faa480d5
Philip Man <202080139@psu.palawan.edu.ph>
2024-05-16 22:30:19 +0800

    update update

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="assets/img/cap-log.png" rel="icon">
+
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
@@ -35,10 +35,8 @@
 <style>
     .logo-overlap {
         height: 150px;
-        margin-top: -60px;
-        /* Adjust this value as needed */
+        margin-top: -80px;
         border: 5px solid white;
-        /* Optional: adds a border to make the logo stand out */
     }
 </style>
 
@@ -55,49 +53,51 @@
                                         <p class="text-center small">Enter your username & password to login</p> -->
                                 <div class="pb-0">
                                     <div class="image">
-                                        <img src="./assets/img/tiniguiban.jpg" alt="..." class="img-fluid"
+                                        <img src="./assets/img/tiniguiban.jpg" alt="..." class="img-fluid rounded"
                                             style="height: 160px; width: 100%;">
                                     </div>
-                                    <div class="card-body">
+                                    <div class="card-body pb-5">
                                         <div class="author logo-overlap d-flex flex-column align-items-center">
                                             <img class="avatar border-gray m-2 rounded-circle"
                                                 src="./assets/img/logo.jpg" alt="..."
-                                                style="height: 100px; margin: 20;">
+                                                style="height: 120px; margin: 20;">
                                             <h5 class="title" style="color: #729ED9;">Welcome to ACI-BT!</h5>
                                             <label for="">Login</label>
                                         </div>
                                     </div>
                                 </div>
-
-                                <form class="row g-3 needs-validation p-3 pt-0" action="login_process.php" method="post"
+                                <form class="row g-3 needs-validation p-3 pt-3" action="login_process.php" method="post"
                                     novalidate>
                                     <div class="col-12">
                                         <label for="yourUsername" class="form-label">Username</label>
                                         <div class="input-group has-validation">
                                             <span class="input-group-text" id="inputGroupPrepend">@</span>
                                             <input type="text" name="username" class="form-control" id="yourUsername"
-                                                required>
+                                                placeholder="Enter Username" required>
                                             <div class="invalid-feedback">Please enter your username.</div>
                                         </div>
                                     </div>
                                     <div class="col-12">
                                         <label for="yourPassword" class="form-label">Password</label>
-                                        <input type="password" name="password" class="form-control" id="yourPassword"
-                                            required>
-                                        <div class="invalid-feedback">Please enter your password!</div>
+                                        <div class="input-group has-validation ">
+                                            <span class="input-group-text" id="inputGroupPrepend"><i
+                                                    class="bi bi-lock"></i></span>
+                                            <input type="password" name="password" class="form-control"
+                                                id="yourPassword" placeholder="Enter Password" required>
+                                            <div class="invalid-feedback">Please enter your password!</div>
+                                        </div>
                                     </div>
+
                                     <div class="col-12">
                                         <button class="btn btn-primary w-100" type="submit">Login</button>
                                     </div>
                                     <div class="col-12">
-                                        <p class="small mb-0">Don't have account? <a href="pages-register.html">Create
+                                        <p class="small mb-0 text-center">Don't have account? <a
+                                                href="pages-register.html">Create
                                                 an account</a></p>
                                     </div>
                                 </form>
                             </div>
-                        </div>
-                        <div class="credits text-center">
-                            Designed by <a href="about.html">&copy; Cayabyabers</a>
                         </div>
                     </div>
             </section>
