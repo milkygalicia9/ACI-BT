c36a4dc96e0ac8764cbcad5c392b88c851dbbdc2
Kristian Marc Anthony Mendoza Ng <202080191@psu.palawan.edu.ph>
2024-05-16 16:59:49 +0800

    update db

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ $password = "uYmm?8E&FQVtpZF";
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
