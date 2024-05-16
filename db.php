d54034c1c4fa0fd838ff998d6398a4d438d76e2c
Kristian Marc Anthony Mendoza Ng <202080191@psu.palawan.edu.ph>
2024-05-16 16:58:04 +0800

    database

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "sql.freedb.tech";
+$username = "freedb_mrcxng";
+$password = "uYmm?8E&FQVtpZF";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
