e38d3671d2e1cc533a6500e5f40e40d2bd82f33e
Philip Man <202080139@psu.palawan.edu.ph>
2024-05-16 20:25:45 +0800

    update all

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>ACI-BT | Home</title>
+    <title>ACI-BT | About</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
