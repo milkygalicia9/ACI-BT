fa2c5adf4369fce077a0b2b89e5cc0c60ebafbf9
Philip Man <202080139@psu.palawan.edu.ph>
2024-05-23 20:57:02 +0800

    update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 <style>
     .logo-overlap {
         height: 150px;
-        margin-top: -80px;
+        margin-top: -90px;
         border: 5px solid white;
     }
 </style>
@@ -47,30 +47,30 @@
             <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center">
                 <div class="container">
                     <div class="row d-flex align-items-center justify-content-center">
-                        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                            <div class="card">
+                        <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
+                            <div class="card" style="height: 740px;">
                                 <!-- <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                                         <p class="text-center small">Enter your username & password to login</p> -->
-                                <div class="pb-0">
+                                <div class="pb-5">
                                     <div class="image">
                                         <img src="./assets/img/tiniguiban.jpg" alt="..." class="img-fluid rounded"
-                                            style="height: 160px; width: 100%;">
+                                            style="height: 240px; width: 100%;">
                                     </div>
                                     <div class="card-body pb-5">
                                         <div class="author logo-overlap d-flex flex-column align-items-center">
                                             <img class="avatar border-gray m-2 rounded-circle"
                                                 src="./assets/img/logo.jpg" alt="..."
-                                                style="height: 120px; margin: 20;">
-                                            <h5 class="title" style="color: #729ED9;">Welcome to ACI-BT!</h5>
-                                            <label for="">Login</label>
+                                                style="height: 160px; margin: 20;">
+                                            <h5 class="title fs-2" style="color: #729ED9;">Welcome to ACI-BT!</h5>
+                                            <label for="" class="fs-2">Login</label>
                                         </div>
                                     </div>
                                 </div>
-                                <form class="row g-3 needs-validation p-3 pt-3" action="login_process.php" method="post"
+                                <form class="row g-3 needs-validation p-5 pt-4" action="login_process.php" method="post"
                                     novalidate>
                                     <div class="col-12">
-                                        <label for="yourUsername" class="form-label">Username</label>
-                                        <div class="input-group has-validation">
+                                        <label for="yourUsername" class="form-label fs-5">Username</label>
+                                        <div class="input-group has-validation" style="height: 50px;">
                                             <span class="input-group-text" id="inputGroupPrepend">@</span>
                                             <input type="text" name="username" class="form-control" id="yourUsername"
                                                 placeholder="Enter Username" required>
@@ -78,8 +78,8 @@
                                         </div>
                                     </div>
                                     <div class="col-12">
-                                        <label for="yourPassword" class="form-label">Password</label>
-                                        <div class="input-group has-validation ">
+                                        <label for="yourPassword" class="form-label fs-5">Password</label>
+                                        <div class="input-group has-validation" style="height: 50px;">
                                             <span class="input-group-text" id="inputGroupPrepend"><i
                                                     class="bi bi-lock"></i></span>
                                             <input type="password" name="password" class="form-control"
@@ -89,7 +89,7 @@
                                     </div>
 
                                     <div class="col-12">
-                                        <button class="btn btn-primary w-100" type="submit">Login</button>
+                                        <button class="btn btn-primary w-100 fs-4" type="submit" style="height: 50px;">Login</button>
                                     </div>
                                 </form>
                             </div>
