59839b29efab42c48343983fda9a6279cf6bdb6c
Philip Man <202080139@psu.palawan.edu.ph>
2024-10-09 20:05:43 +0800

    philip_docs cert employability

--- includes/cert_db.php
+++ includes/cert_db.php
@@ -122,7 +122,6 @@ if (isset($_POST["business_permit_new"])) {
 }
 //   End of Business Permit New
 
-
 // Business Permit Renew
 if (isset($_POST["business_permit_renew"])) {
     // Sanitize and assign form data to variables
@@ -181,6 +180,75 @@ if (isset($_POST["business_permit_renew"])) {
 }
 // End of Business Permit Renew
 
+// Certificate of Employability
+if (isset($_POST["certificate_of_employability"])) {
+    // Sanitize and assign form data to variables
+    $first_name = $conn->real_escape_string($_POST["first_name"]);
+    $middle_initial = $conn->real_escape_string($_POST["middle_initial"]);
+    $last_name = $conn->real_escape_string($_POST["last_name"]);
+    $suffix = $conn->real_escape_string($_POST["suffix"]);
+    $age = $conn->real_escape_string($_POST["age"]);
+    $address = $conn->real_escape_string($_POST["purok"]);
+    //$issued_date = $conn->real_escape_string($_POST["issued_date"]);
+    //$duty_officer_name = $conn->real_escape_string($_POST["duty_officer_name"]);
+
+    // Define SQL query using prepared statements for the business permit
+    $fullname = $first_name . ' ' . $middle_initial . ' ' . $last_name . ' ' . $suffix;
+    $fullname = ucwords($fullname);
+    $duty_officer_name = $_SESSION['username'];
+    $issued_date = date('Y-m-d');
+    $stmt = $conn->prepare("INSERT INTO certificate_of_employability (fullname, age, address, issued_date, duty_officer_name) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param('sssss', $fullname, $age, $address, $issued_date, $duty_officer_name);
+
+    // Execute the business permit insertion query
+    if ($stmt->execute()) {
+        echo "New certificate of employability record inserted successfully";
+
+        // Fetch admin ID
+        $sql = "SELECT id FROM admin WHERE username = ?";
+        $admin_stmt = $conn->prepare($sql);
+        $admin_stmt->bind_param('s', $_SESSION['username']);
+        $admin_stmt->execute();
+        $admin_result = $admin_stmt->get_result();
+
+        // Check if the admin user was found
+        if ($admin_result->num_rows > 0) {
+            $row = mysqli_fetch_assoc($admin_result);
+            $admin_id = $row['id'];
+
+            // Insert a transaction record into the `transactions` table
+            $trans_stmt = $conn->prepare("INSERT INTO transactions (transact_by, doc_id, fullname, client_trans_id, created_at) VALUES (?, 2, ?,(SELECT COUNT(*) FROM business_permit_renew), NOW())");
+            $trans_stmt->bind_param('is', $admin_id, $fullname);
+
+            // Execute the transaction query
+            if ($trans_stmt->execute()) {
+                echo "Transaction record inserted successfully";
+            } else {
+                echo "Error: " . $trans_stmt->error;
+            }
+
+            $trans_stmt->close();
+        } else {
+            echo "Error: Admin user not found.";
+        }
+
+        $admin_stmt->close();
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close database connection
+    $stmt->close();
+    $conn->close();
+}
+// End of Certificate of Employability 
+
+
+
+
+
+
+
 // Certificate of Transfer
 if (isset($_POST["transfer_of_residency"])) {
     // Sanitize and assign form data to variables
@@ -202,7 +270,7 @@ if (isset($_POST["transfer_of_residency"])) {
     $issued_date = date('Y-m-d');
 
     $stmt = $conn->prepare("INSERT INTO transfer_of_residency (fullname, address, nationality, civil_status, previous_address, purpose, issued_date) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param('sssssss', $fullname, $address, $nationality, $civil_status, $previous_address, $purpose,  $issued_date);
+    $stmt->bind_param('sssssss', $fullname, $address, $nationality, $civil_status, $previous_address, $purpose, $issued_date);
 
 
     // Execute the certificate of transfer insertion query
